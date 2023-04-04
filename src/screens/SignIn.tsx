43f9ea51b11e6cd4127026346eb91369ebcebe15
GABI <gabriellimrqs1998@gmail.com>
2023-04-04 12:14:07 -0300

    yupsignin

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -5,7 +5,22 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes'; //Essa é a propriedade que exportamos no auth e importamos aqui para acesssar as rotas.
 import { useNavigation } from '@react-navigation/native';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
+
+type FormDataProps = { //1º crio a tipagem
+    email: string;
+    password: string;
+}
+
+const {control, handleSubmit } = useForm<FormDataProps>({ //4º faço a desestruturação e passo o useForm para retornar os valores em array de objetos.
+
+});
+
+function handleSignIn( { email, password} : FormDataProps ) {
+    console.log({email, password}); //2º dou log no email e na senha 
+}
 
 export function SignIn(){
 
@@ -19,7 +34,9 @@ export function SignIn(){
 
     return (
         <ScrollView contentContainerStyle={{flexGrow:1}} showsVerticalScrollIndicator={false} >
+
             <VStack flex={1} px={10} pb={16}>
+
                 <Image 
                 source={BackgroundImg}
                 defaultSource={BackgroundImg}
@@ -33,24 +50,45 @@ export function SignIn(){
                     Treine sua mente e o seu corpo
                 </Text>
                 </Center>
+
                 <Center>
                 <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
                     Acesse sua Conta
                 </Heading>
+
+                <Controller //3º importo o controller que controla o meu input
+                control={ control }
+                name="email"
+                render={({ field : { onChange, value } }) => (            
                 <Input 
-                placeholder="E-mail" 
-                keyboardType="email-address"
-                autoCapitalize="none"
+                    placeholder="E-mail" 
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    onChangeText={onChange}
+                    value={value}
+                />  
+                )}
                 />
+
+                <Controller
+                control={control}
+                name="email"
+                render={({ field : { onChange, value}}) => (
                 <Input 
                 placeholder="Senha"  
                 secureTextEntry
+                onChangeText={onChange}
+                value={value}
                 />
+                )}
+                />
+
                 <Button  
                 title="Acessar"
                 variant="solid"
                 />
                 </Center>
+
                 <Center>
                 <Text 
                 color="gray.100" 
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,5 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input';
@@ -8,6 +8,7 @@ import {AuthNavigatorRoutesProps} from '@routes/auth.routes'
 import { useNavigation} from '@react-navigation/native'
 import { useForm, Controller } from 'react-hook-form'; //lib para pegar o valor dos inputs, e o Controller vai controlar nosso inputs
 import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type FormDataProps =  {
     name: string,
@@ -17,16 +18,19 @@ type FormDataProps =  {
 } //passei a tipagem dos dados que estou enviando do formulário.
 
 const signUpSchema = yup.object({
-    name: yup.string().required("Informe o nome"),
-    email: yup.string().required("").email("E-mail inválido"),
-}).required
+    name: yup.string().required("Informe o nome."),
+    email: yup.string().required("Informe o e-mail.").email("E-mail inválido."),
+    password: yup.string().required("Informe a senha.").min(6, "A senha deve ter pelo menos 6 dígitos."),
+    passwordConfirm: yup.string().required("Confirme a senha.").oneOf([yup.ref('password')], 'A confirmação da senha não confere.') //o valor do campo deve ser igual ao valor do campo "password".
+});
 
 export function SignUp(){
 
 
     const {control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
-        defaultValues:{} //
-    }); //passamos a props dos dados, aqui onde são enviados pro form.
+        resolver: yupResolver(signUpSchema)
+    }); 
+    //passamos a props dos dados, aqui onde são enviados pro form.
     //vamos usar o useForm para acessar o control, passamos o mesmo control para 
     //todos inputs pois vão ser controlados pelo mesmo formulário.
     //vamos usar o handleSubmit para ele enviar os datas da nossa aplicação.                                            .
@@ -65,9 +69,9 @@ export function SignUp(){
                 <Controller  //controla o input
                 control={control}  //controla o valor de cada input
                 name="name" // nome do input
-                rules={{
-                    required: "Informe o nome."
-                }}
+                // rules={{
+                //     required: "Informe o nome."
+                // }} POIS JÁ ADICIONAMOS O SCHEMA
                 render={({ field : { onChange, value } }) => ( // e para cada input controlado eu digo qual input quero renderizar
                     <Input 
                     placeholder="Nome" 
@@ -83,13 +87,6 @@ export function SignUp(){
                 <Controller 
                 control={control}
                 name="email"
-                rules={{
-                    required: "Informe o email.",
-                    pattern: {
-                        value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                        message: 'E-mail inválido'
-                      }
-                }}
                 render={({ field : {onChange, value} }) => (
                     <Input 
                     placeholder="E-mail" 
@@ -112,6 +109,7 @@ export function SignUp(){
                     secureTextEntry
                     onChangeText={onChange}
                     value={value}
+                    errorMessage={errors.password?.message}
                     />
                 )}
                 />
@@ -127,6 +125,7 @@ export function SignUp(){
                     value={value}
                     onSubmitEditing={handleSubmit(handleSignUp)} //para ahabilitar o botão de envio do formulário
                     returnKeyType="send"
+                    errorMessage={errors.passwordConfirm?.message}
                     />
                 )}
                 />
